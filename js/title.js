dfa1b4ae4c1a3b0acccb42a20ea78591e45c318b
zilboe <1149845493@qq.com>
2023-08-10 18:34:25 +0800

    Site updated: 2023-08-10 18:34:24

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ document.addEventListener("visibilitychange", function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = "♪(^∇^*)欢迎肥来！" + OriginTitile;
+    document.title = "♪(^∇^*)欢迎回来！" + OriginTitile;
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
