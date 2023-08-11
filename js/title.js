6f070bb19f4fe17364d723fb24cf431b37470629
zilboe <1149845493@qq.com>
2023-08-11 18:00:36 +0800

    Site updated: 2023-08-11 18:00:36

--- js/title.js
+++ js/title.js
@@ -1,14 +1,14 @@
 //动态标题
 var OriginTitile = document.title;
 var titleTime;
-document.addEventListener("visibilitychange", function () {
+document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = "w(ﾟДﾟ)w 不要走！再看看嘛！";
+    document.title = '👀跑哪里去了~';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = "♪(^∇^*)欢迎回来！" + OriginTitile;
+    document.title = '🐖抓到你啦～';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
