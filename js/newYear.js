a92a6d5208dac3ace9543dab364936d0817f725a
zilboe <1149845493@qq.com>
2023-08-12 14:09:41 +0800

    Site updated: 2023-08-12 14:09:41

--- js/newYear.js
+++ js/newYear.js
@@ -60,5 +60,5 @@ var newYear = () => {
 }
 // Pjax适配：若没有开启Pjax这里直接是newYear()即可
 // 开了Pjax的用以下两句
-document.addEventListener('pjax:complete', newYear);
-document.addEventListener('DOMContentLoaded', newYear);
+// document.addEventListener('pjax:complete', newYear);
+// document.addEventListener('DOMContentLoaded', newYear);
