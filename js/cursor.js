91b361d5131b90668c60e9ed4e773e9a9ef68510
zilboe <1149845493@qq.com>
2023-08-16 21:49:20 +0800

    Site updated: 2023-08-16 21:49:20

--- js/cursor.js
+++ js/cursor.js
@@ -88,3 +88,17 @@ class Cursor {
   CURSOR = new Cursor();
   // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+new Vue({
+  data: function () {
+      this.$notify({
+          title: "你已被发现😜",
+          message: "小伙子，扒源记住要遵循GPL协议！",
+          position: 'top-left',
+          offset: 50,
+          showClose: true,
+          type: "warning",
+          duration: 5000
+      });
+  }
+})
